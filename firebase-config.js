d19d11d48f5f1c6b270b99908c23c8f1aff387af
akinnevmo-gif <sokpahakinsaye@gmail.com>
2026-03-04 12:07:02 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
-// Replace with your Firebase project config
+// Replace YOUR_KEYs with your Firebase project keys
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "spotliberia.firebaseapp.com",
-  projectId: "spotliberia",
-  storageBucket: "spotliberia.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  authDomain: "bionurse-pro.firebaseapp.com",
+  projectId: "bionurse-pro",
+  storageBucket: "bionurse-pro.appspot.com",
+  messagingSenderId: "262309708872",
+  appId: "1:262309708872:web:c8200a831bbb51a7fc447d"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
